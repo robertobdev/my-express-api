7a39e76db9d58611a7d254768bebc825c752e3e0
Roberto Barbosa <robertojf@gmail.com>
2021-08-01 15:26:55 -0300

    Configue default response and exception

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomExceptionFilter } from './filters/exception.filter';
+import { CustomValidationPipe } from './pipes/custom-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new CustomValidationPipe());
+  app.useGlobalFilters(new CustomExceptionFilter());
+  app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
 bootstrap();
