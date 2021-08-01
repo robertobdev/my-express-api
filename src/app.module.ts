4c069c945fda57e80a7cf6dd8ed4f22985108037
Roberto Barbosa <robertojf@gmail.com>
2021-08-01 15:27:07 -0300

    Create module folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
+import { TodoModule } from './modules/todo/todo.module';
 
 @Module({
   imports: [
